4f60b7d70b9f36e5725de2e5ba67a6ca61b35edc
quail <quail@honeycomb.io>
2026-02-02 09:39:16 -0600

    feat: add ECR pull-through-cache to hnypc-artifacts

--- ecr.tf
+++ ecr.tf
@@ -13,3 +13,56 @@ resource "aws_ecr_repository" "this" {
 
   image_tag_mutability = "MUTABLE"
 }
+
+data "aws_iam_policy_document" "ptc_trust_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["pullthroughcache.ecr.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "pull_through_cache" {
+  name               = "pull-through-cache"
+  assume_role_policy = data.aws_iam_policy_document.ptc_trust_policy.json
+}
+
+data "aws_iam_policy_document" "pull_through_cache" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchImportUpstreamImage",
+      "ecr:BatchGetImage",
+      "ecr:GetImageCopyStatus",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:PutImage"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "pull_through_cache" {
+  role   = aws_iam_role.pull_through_cache.name
+  policy = data.aws_iam_policy_document.pull_through_cache.json
+}
+
+locals {
+  hnypc_artifacts_account_id = "098131747737"
+}
+
+resource "aws_ecr_pull_through_cache_rule" "ptc" {
+  ecr_repository_prefix = "ROOT"
+  upstream_registry_url = "${local.hnypc_artifacts_account_id}.dkr.ecr.us-east-1.amazonaws.com"
+  custom_role_arn       = aws_iam_role.pull_through_cache.arn
+}
+
